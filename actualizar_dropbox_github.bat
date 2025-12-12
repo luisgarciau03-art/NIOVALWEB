d1807a50d9b6de713c6157995d50edcacdb4e195
LUIS V <luisht3g@gmail.com>
2025-12-11 18:32:17 -0600

    Primer commit: panel de usuario actualizado

---
+++ actualizar_dropbox_github.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Ejecuta el script de Python para actualizar el token y subir el archivo a GitHub
+cd /d "C:\Users\PC 1\NIOVALWEB"
+"C:\Users\PC 1\AppData\Local\Programs\Python\Python313\python.exe" "import dropbox.py"
+pause
